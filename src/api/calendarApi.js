2659fa74f64bc1db6d051edf528a3607775c27b9
Dennis <dennisb.galeano@gmail.com>
2024-04-23 16:08:15 -0500

    SC26 Cl411 - instalacion de Axios - Para poder hacer peticiones HTTP, configuracion del endpoint con las variables de entorno. creacion folder api_calendarApi.js

---
+++ src/api/calendarApi.js
@@ -0,0 +1,21 @@
+// en la carpeta api "aca" se puede cerear un archivo por endpoint, para autenticaciones y demas. aca solo calendar..
+// si yo utilizo el calendarApi ya va a tener el URL pre configurado
+
+import axios from "axios";
+import { getEnvVariables } from "../helpers/getEnvVariables";  // Aca estan las varuaibles de entorno del front, es una FN que lo que va a retornar son mis variables de entorno
+
+
+const { VITE_API_URL} = getEnvVariables(); // esta variable esta en .env
+
+const calendarApi = axios.create ( { 
+  baseURL: VITE_API_URL //la base del URL, sirva para no tener uqe especificar en todos los lugares la ruta.. se configura por defecto, la url en un principio es la del .env
+});
+
+//todo: configurar uintercemprores
+
+
+export default calendarApi;
+
+//INTERCEPTORES: vienen de los axios, permiten interceptar una petidcion antes o despues de que se haga y añadir o modificar la despuesta o informacion a la peticion
+
+ 
